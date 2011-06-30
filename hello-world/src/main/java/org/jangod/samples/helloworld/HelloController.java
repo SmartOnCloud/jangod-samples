22799fd847cf84dcddfd0edc1bd03435193aedfa
Igor Mihalik <igor.mihalik@gmail.com>
2011-06-30 14:54:18 +0100

    sample hello world created

---
+++ hello-world/src/main/java/org/jangod/samples/helloworld/HelloController.java
@@ -0,0 +1,23 @@
+package org.jangod.samples.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Main application controller
+ * 
+ * @author igor.mihalik
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = { "/index", "" })
+    public String index(ModelMap model,
+	    @RequestParam(required = false) String name) {
+	model.addAttribute("name", name);
+	return "index";
+    }
+
+}
